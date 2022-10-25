44f6e8e353e52877bc3b89d585bb5b6000a97769
Ilya <ilyabelevcev00@gmail.com>
2022-10-25 21:41:16 +0300

    finish

--- script.js
+++ script.js
@@ -1,6 +1,59 @@
+
+
 var app = new Vue({
    el: '#site',
    data: {
-      message: 'Привет, Vue!'
+      items: [
+         {
+            id: 0,
+            title: "Действие из первой зоны",
+            categoryId: 0,
+         },
+         {
+            id: 1,
+            title: "Действие из первой зоны",
+            categoryId: 0,
+         },
+         {
+            id: 2,
+            title: "Действие из второй зоны",
+            categoryId: 1,
+         },
+         {
+            id: 3,
+            title: "Действие из третьей зоны",
+            categoryId: 2,
+         },
+      ],
+      categories: [
+         {
+            id: 0,
+            title: "Первая Зона",
+         },
+         {
+            id: 1,
+            title: "Вторая Зона",
+         },
+         {
+            id: 2,
+            title: "Третья Зона",
+         },
+      ],
+   },
+   methods: {
+      onDragStart: function (e, item) {
+         e.dataTransfer.dropEffect = "move"
+         e.dataTransfer.effectAllowed = "move"
+         e.dataTransfer.setData("itemId", item.id.toString())
+      },
+      onDrop: function(e, categoryId, items) {
+         const itemId = e.dataTransfer.getData("itemId")
+         items = items.map(function(i) {
+            if(i.id == itemId) {
+               i.categoryId = categoryId
+            }
+            return i
+         })
+      },
    }
 })
